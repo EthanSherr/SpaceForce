aecbec051697a3cc1ce130beb92ef1377432a5c3
Ethan Sherr <esherrthan@gmail.com>
2020-02-17 12:21:17 -0600

    initial setup

---
+++ Intermediate/Build/Win64/SpaceForceEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "A:/UE4/projects/SpaceForce/Intermediate/Build/Win64/SpaceForceEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/SpaceForce/SpaceForceGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SPACEFORCE_SpaceForceGameModeBase_generated_h
+#error "SpaceForceGameModeBase.generated.h already included, missing '#pragma once' in SpaceForceGameModeBase.h"
+#endif
+#define SPACEFORCE_SpaceForceGameModeBase_generated_h
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_SPARSE_DATA
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_RPC_WRAPPERS
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASpaceForceGameModeBase(); \
+	friend struct Z_Construct_UClass_ASpaceForceGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASpaceForceGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SpaceForce"), NO_API) \
+	DECLARE_SERIALIZER(ASpaceForceGameModeBase)
+
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASpaceForceGameModeBase(); \
+	friend struct Z_Construct_UClass_ASpaceForceGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASpaceForceGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SpaceForce"), NO_API) \
+	DECLARE_SERIALIZER(ASpaceForceGameModeBase)
+
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASpaceForceGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASpaceForceGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpaceForceGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpaceForceGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpaceForceGameModeBase(ASpaceForceGameModeBase&&); \
+	NO_API ASpaceForceGameModeBase(const ASpaceForceGameModeBase&); \
+public:
+
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASpaceForceGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpaceForceGameModeBase(ASpaceForceGameModeBase&&); \
+	NO_API ASpaceForceGameModeBase(const ASpaceForceGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpaceForceGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpaceForceGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASpaceForceGameModeBase)
+
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_12_PROLOG
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_SPARSE_DATA \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_RPC_WRAPPERS \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_INCLASS \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_SPARSE_DATA \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SPACEFORCE_API UClass* StaticClass<class ASpaceForceGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SpaceForce_Source_SpaceForce_SpaceForceGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
